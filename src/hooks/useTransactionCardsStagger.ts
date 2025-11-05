0ad7b99f3cc02453b9603a8ae2df7b7894e2c5b9
stefanstudiodirection <stefan.radovic@studiodirection.com>
2025-11-05 09:25:11 +0100

    transaction cards

--- src/hooks/useTransactionCardsStagger.ts
+++ src/hooks/useTransactionCardsStagger.ts
@@ -3,28 +3,28 @@ import gsap from "gsap";
 import { RefObject } from "react";
 
 export const useTransactionCardsStagger = (
-  containerRef: RefObject<HTMLElement>,
-  dependency?: any 
+	containerRef: RefObject<HTMLElement>,
+	dependency?: any
 ) => {
-  useGSAP(
-    () => {
-      const cards = containerRef.current?.querySelectorAll(".transaction-item");
-      
-      if (!cards || cards.length === 0) return;
+	useGSAP(
+		() => {
+			const cards = containerRef.current?.querySelectorAll(".transaction-item");
 
-      gsap.fromTo(
-        cards,
-        {
-          y: -24,
-        },
-        {
-          y: 0,
-          duration: 0.3,
-          ease: "power2.out",
-          stagger: 0.1,
-        }
-      );
-    },
-    { scope: containerRef, dependencies: [dependency] }
-  );
+			if (!cards || cards.length === 0) return;
+
+			gsap.fromTo(
+				cards,
+				{
+					opacity: 0,
+				},
+				{
+					opacity: 1,
+					duration: 0.3,
+					ease: "power2.out",
+					stagger: 0.1,
+				}
+			);
+		},
+		{ scope: containerRef, dependencies: [dependency] }
+	);
 };
